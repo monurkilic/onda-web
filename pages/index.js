6e44d88ed660c1c5f85b0e4855b3f1005c78b239
monurkilic <32382706+monurkilic@users.noreply.github.com>
2026-03-25 18:44:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-export default function Home() {
-  return (
-    <div style={{backgroundColor: '#0a192f', color: '#d4af37', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>
-      <h1 style={{fontSize: '4rem', fontStyle: 'italic'}}>ONDA YATIRIM</h1>
-      <p style={{color: '#8e8e8e', fontSize: '1.5rem'}}>Güvenin Yeni Dalgası</p>
-      <a href="/studio" style={{marginTop: '20px', color: '#fff', textDecoration: 'underline'}}>Yönetim Paneline Git</a>
-    </div>
-  )
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../sanity.config'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
 }
