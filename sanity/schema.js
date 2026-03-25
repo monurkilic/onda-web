243eff11b7cdd1943108d3f3bb1906d06416c237
monurkilic <32382706+monurkilic@users.noreply.github.com>
2026-03-25 19:58:23 +0300

    Update schema.js

--- sanity/schema.js
+++ sanity/schema.js
@@ -3,9 +3,46 @@ export default {
   title: 'İlanlar',
   type: 'document',
   fields: [
-    { name: 'title', title: 'Başlık', type: 'string' },
-    { name: 'location', title: 'Konum', type: 'string' },
-    { name: 'price', title: 'Fiyat', type: 'string' },
-    { name: 'analysis', title: 'Onda Analizi', type: 'text' }
-  ]
+    {
+      name: 'title',
+      title: 'İlan Başlığı',
+      type: 'string',
+    },
+    {
+      name: 'location',
+      title: 'Konum (Şehir/İlçe)',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Fiyat',
+      type: 'string',
+    },
+    {
+      name: 'currency',
+      title: 'Para Birimi',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'TL', value: 'TL' },
+          { title: 'GBP', value: 'GBP' },
+          { title: 'USD', value: 'USD' },
+          { title: 'EUR', value: 'EUR' },
+        ],
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Ana Resim (Kapak)',
+      type: 'image',
+      options: {
+        hotspot: true, // Bu sayede resmi panelde kırpabilirsin
+      },
+    },
+    {
+      name: 'analysis',
+      title: 'Onda Analizi',
+      type: 'text',
+    },
+  ],
 }
