5140e936d65d35c0b2fb25bb2d5dbf73eb8f82cc
monurkilic <32382706+monurkilic@users.noreply.github.com>
2026-03-26 10:32:27 +0300

    Update schema.js

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,7 +1,19 @@
-import property from './property'
-import post from './post'
+import property from './property.js'
+import post from './post.js'
 
 export const schemaTypes = [
   property,
   post
 ]
+
+// Bazı Sanity sürümleri "default" export bekleyebilir, o yüzden bunu da ekliyoruz:
+export default schemaTypesimport property from './property.js'
+import post from './post.js'
+
+export const schemaTypes = [
+  property,
+  post
+]
+
+// Bazı Sanity sürümleri "default" export bekleyebilir, o yüzden bunu da ekliyoruz:
+export default schemaTypes
