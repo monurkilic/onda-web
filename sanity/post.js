94fe35cfc89584e2439ae4fe8691ce5348d6f7f2
monurkilic <32382706+monurkilic@users.noreply.github.com>
2026-03-26 10:27:56 +0300

    Update post.js

--- sanity/post.js
+++ sanity/post.js
@@ -3,11 +3,51 @@ export default {
   title: 'Blog Yazıları',
   type: 'document',
   fields: [
-    { name: 'title', title: 'Başlık', type: 'string' },
-    { name: 'slug', title: 'URL Uzantısı (Slug)', type: 'slug', options: { source: 'title' } },
-    { name: 'mainImage', title: 'Kapak Görseli', type: 'image', options: { hotspot: true } },
-    { name: 'publishedAt', title: 'Yayınlanma Tarihi', type: 'datetime' },
-    { name: 'body', title: 'İçerik', type: 'array', of: [{ type: 'block' }, { type: 'image' }] },
-    { name: 'excerpt', title: 'Kısa Özet', type: 'text', rows: 3 }
-  ]
+    {
+      name: 'title',
+      title: 'Blog Başlığı',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Blog Linki (Slug)',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Kapak Görseli',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Yayınlanma Tarihi',
+      type: 'datetime',
+    },
+    {
+      name: 'excerpt',
+      title: 'Kısa Özet',
+      type: 'text',
+      description: 'Blog listesinde görünecek kısa açıklama (maksimum 3 satır).',
+      rows: 3,
+    },
+    {
+      name: 'body',
+      title: 'İçerik',
+      type: 'array',
+      of: [
+        { type: 'block' },
+        { 
+          type: 'image', 
+          options: { hotspot: true } 
+        }
+      ],
+    },
+  ],
 }
